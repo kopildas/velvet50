2c26fd4893562a9aada310f6f8e23398cef5a5c2
Swadhin941 <swadhinghosh@Dorik23s-MacBook-Air.local>
2024-12-04 00:23:42 +0600

    Database connection

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const { port } = require("./config/config");
+const { port, dbConnect } = require("./config/config");
 
 app.use(cors());
 app.use(express.json());
 
 
-app.listen(port, ()=>{
+app.listen(port, async()=>{
     console.log(`Server running on port ${port}`);
+    await dbConnect();
 })
--- frontend/app/component/Footer/Footer.jsx
+++ frontend/app/component/Footer/Footer.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
-
+import "./Footer.css";
 const Footer = () => {
   return (
-    <div>
-      
+    <div className='container'>
+      <div className="grid grid-cols-6 gap-2">
+          <div>
+              <h1 style={{fontSize:"400"}}>VELVET 50.</h1>
+          </div>
+      </div>
+      <div className="grid grid-cols-6 gap-2">
+
+      </div>
     </div>
   )
 }
