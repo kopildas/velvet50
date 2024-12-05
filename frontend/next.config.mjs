0555ebf08ba546df2d7af7a3d9c5500fe0260515
kopildas <102140363+kopildas@users.noreply.github.com>
2024-12-05 17:51:57 +0600

    fix: next js cach realted issue atmt 2

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    async headers() {
+      return [
+        {
+          source: '/:path*',
+          headers: [
+            {
+              key: 'Cache-Control',
+              value: 'no-store', // Forces no caching
+            },
+          ],
+        },
+      ];
+    },
+  };
+  
+  export default nextConfig;
